b46a5fd62c42e555a0c7c823d59f9e134dd11722
Greg Chattin-McNichols <gchattinmcnichols@pivotal.io>
2016-07-07 15:37:14 -0700

    Hit API with hardcode params
    Signed-off-by: Jonathan Irwin <jonathan.irwin@carinacare.com@pivotal.io>

--- app/services/student_info_api.rb
+++ app/services/student_info_api.rb
@@ -1,25 +1,32 @@
 class StudentInfoApi
 
   def self.apiquery
-    # require 'uri'
-    # require 'net/http'
-    #
-    # url = URI("https://api.myseiubenefits.org/findname")
-    #
-    # http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    #
-    # request = Net::HTTP::Post.new(url)
-    # request["content-type"] = 'application/x-www-form-urlencoded'
-    # request["cache-control"] = 'no-cache'
-    # request["postman-token"] = 'c80f4017-0aec-4b0a-fd4d-f9ba6f9b9b4e'
-    # request.body = "firstName=TROY&lastName=CORNYN&dob=1973-08-28&last4=0058"
-    #
-    # response = http.request(request)
-    #
-    # @response = response.read_body
-    StudentInfoApi.new('Bob2', 'Loblaw2', 'nope2', 'Somewhere2', 2)
+    require 'rubygems'
+    require 'json'
+    require 'uri'
+    require 'net/http'
+
+    url = URI("https://api.myseiubenefits.org/findname")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(url)
+    request["content-type"] = 'application/x-www-form-urlencoded'
+    request["cache-control"] = 'no-cache'
+    request["postman-token"] = 'c80f4017-0aec-4b0a-fd4d-f9ba6f9b9b4e'
+    request.body = "firstName=TROY&lastName=CORNYN&dob=1973-08-28&last4=0058"
+
+    response = http.request(request)
+
+    parsed = JSON.parse(response.read_body)
+    student = parsed['student'][0]
+    StudentInfoApi.new(student['firstname'],
+                       student['lastname'],
+                       student['compliance_status'],
+                       student['mailingstreet'],
+                       student['studentid'])
   end
 
   def initialize(first, last, compliance, address, id)
@@ -45,7 +52,7 @@ class StudentInfoApi
   def address
     @address
   end
-  
+
   def student_id
     @student_id
   end
